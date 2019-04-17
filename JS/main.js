2b43f6570fafe19b234e66e1824b20854a8ea3c1
mizzta <ritatung@hotmail.com>
2019-04-17 16:13:27 -0700

    category is done. text in the middle. responsing to media queries

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,48 @@
-const nameEl = document.getElementById('name');
-console.log(nameEl);
-const name = prompt("Please enter a name");
-nameEl.innerHTML = name;
+document.addEventListener("DOMContentLoaded", function () {
+    // all of your JS code goes here
 
 
+    const nameEl = document.getElementById('name');
+    console.log(nameEl);
+    nameEl.innerHTML = name;
+
+
+    const evolveBtn = document.getElementById('evolve');
+
+    let currentStage = 'Simple Spore';
+    let clicks = 1;
+
+    // Add the initial stage of evolution to the span id="evolution"
+
+    evolveBtn.addEventListener("click", function evolve() {
+
+
+        // add 1 to the clicks variable
+        clicks = clicks + 1;
+        //clicks++; 
+
+
+        // When the number of clicks reaches 10, add a second stage of evolution to the DOM!
+        if (clicks === 10) {
+
+            // You choose the name of each additional stage.
+            currentStage = "small larvae";
+        }
+
+        if (clicks > 10) {
+            currentStage = "2nd";
+        }
+
+        if (clicks > 20) {
+            currentStage = "3rd";
+        }
+
+
+        console.log("clicks is now " + clicks);
+        console.log("currentStage is now " + currentStage);
+
+
+
+    });
+
+});
