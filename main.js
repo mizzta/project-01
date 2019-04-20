6e426a19d8dc8424757084899d39770269b3f7d9
mizzta <ritatung@hotmail.com>
2019-04-19 22:07:06 -0700

    Change text styles and footer

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    
+    // Allf of your code goes inside here
+    
+    console.log('this is working');
+
+
+    const flkty = new Flickity( '.main-carousel', {
+        cellAlign: 'left',
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+      });
+
+
+
+    
+}); 
+//end of document ready
